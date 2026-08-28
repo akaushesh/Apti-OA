df579cc6c6c6678e6160f85b28adc62f73793fd8
akaushesh <anik.kaushesh@gmail.com>
2026-08-29 00:14:35 +0530

    v4 - Iincrease express body size limit from 16kb to 1mb

--- server/src/app.js
+++ server/src/app.js
@@ -23,8 +23,8 @@ app.use(
 );
 
 
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.json({limit:"1mb"}))
+app.use(express.urlencoded({extended:true,limit:"1mb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
